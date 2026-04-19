94387c9d7b4df8963be5a8f6c59f5369ae825abb
karasidnez <karasdnez@gmail.com>
2026-04-19 14:37:32 +0300

    Step 12: Add weather index calculation with Dispatchers.Default

--- app/src/main/java/com/timonin/weatherdashboard/MainActivity.kt
+++ app/src/main/java/com/timonin/weatherdashboard/MainActivity.kt
@@ -110,6 +110,14 @@ fun WeatherDashboardScreen(
             value = weatherState.humidity?.let {"$it m/s"} ?: "-",
             isLoading = weatherState.isLoading && weatherState.windSpeed == null
         )
+        if (weatherState.weatherIndex != null){
+            WeatherCard(
+                emoji = "📊",
+                title = "Wether Index",
+                value = "${weatherState.weatherIndex}",
+                isLoading = false
+            )
+        }
 
         Spacer(modifier = Modifier.height(32.dp))
         Button(
